4600874654752c771deb0f952f240d5eacf98f00
NamPV <nampv686@gmail.com>
2023-04-27 13:10:06 +0700

    Set up Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+
+  return (
+    <div className="app-container">
+      <button class="btn btn-primary">Primary</button>
+    </div>
+  );
+}
+
+export default App;
