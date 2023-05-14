9556dbc04096fa67b6ce40cd04b7f019abcbc6a4
NamPV686 <nampv686@gmail.com>
2023-05-14 16:53:55 +0700

    #95. Tao Face data cho giao dien

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -3,6 +3,8 @@ import Select from 'react-select';
 import './Questions.scss';
 import {BiImageAdd} from 'react-icons/bi'
 import { BsPatchPlusFill, BsFillPatchMinusFill } from 'react-icons/bs';
+import { v4 as uuidv4 } from 'uuid';
+import _ from 'lodash';
 
 const Questions = (props) => {
     const options = [
@@ -12,6 +14,72 @@ const Questions = (props) => {
       ];
 
     const [ selectedQuiz, setSelectedQuiz ] = useState();
+
+    const [ questions, setQuestions ] = useState(
+        [
+            {
+                id: uuidv4(),
+                description: 'questions 1',
+                imageFile: '',
+                imageName: '',
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: 'answer 1',
+                        isCorrect: false
+                    }
+                ]
+            }
+        ]
+    );
+
+    const handleAddRemoveQuestion = (type, id) => {
+        if(type === 'ADD'){
+            const newQuestion = {
+                id: uuidv4(),
+                description: '',
+                imageFile: '',
+                imageName: '',
+                answers: [
+                    {
+                        id: uuidv4(),
+                        description: 'answer 1',
+                        isCorrect: false
+                    }
+                ]
+            }
+            
+            setQuestions([...questions, newQuestion]);
+        }
+
+        if(type === 'REMOVE'){
+            let questionClone = _.cloneDeep(questions);
+            questionClone = questionClone.filter(item => item.id !== id);
+            setQuestions(questionClone);
+        }
+    }
+
+    const handleAddRemoveAnswer = (type, questionId, answerId) => {
+        let questionClone = _.cloneDeep(questions);
+
+        if(type === 'ADD'){
+            const newAnswer = {
+                id: uuidv4(),
+                description: 'answer 1',
+                isCorrect: false
+            }
+
+            let index = questionClone.findIndex(item => item.id === questionId);
+            questionClone[index].answers.push(newAnswer);
+            setQuestions(questionClone);
+        }
+
+        if(type === 'REMOVE'){
+            let index = questionClone.findIndex(item => item.id === questionId);
+            questionClone[index].answers = questionClone[index].answers.filter(item => item.id !== answerId)
+            setQuestions(questionClone);
+        }
+    }
 
     return(
         <div className="questions-container">
@@ -29,46 +97,77 @@ const Questions = (props) => {
                     />
                 </div>
                 <div>Add Question</div>
-                <div>
-                    <div className='question-content'>
-                        <div className="col-md-6">
-                            <input 
-                            type="text" 
-                            className="form-control"
-                            />
-                        </div>
-                        <div className="col-md-2">
-                            <label className="form-label label-upload" htmlFor='labelUpload'><BiImageAdd className='icon-image'/>Upload File Image</label>
-                            <input 
-                            type='file' 
-                            hidden
-                            id='labelUpload'
-                            />
-                        </div>
-                        <div className='col-md-5 btn-add'>
-                            <span><BsPatchPlusFill className='icon-add'/></span>
-                            <span><BsFillPatchMinusFill className='icon-delete'/></span>
-                        </div>
-                    </div>
-                    <div className='answer-content mt-3'>
-                        <div className="form-check mt-1">
-                            <input className="form-check-input" 
-                                type="checkbox"
-                            />
-                        </div>
-                        <div className="col-md-6">
-                            <input
-                            type="text" 
-                            placeholder='Answer 1'
-                            className="form-control"
-                            />
-                        </div>
-                        <div className='col-md-5 btn-add'>
-                            <span><BsPatchPlusFill className='icon-add'/></span>
-                            <span><BsFillPatchMinusFill className='icon-delete'/></span>
-                        </div>
-                    </div>
-                </div>
+                {
+                    questions && questions.length > 0
+                    && questions.map((question, index) => {
+                        return(
+                            <div key={question.id} className='q-main mb-3'>
+                                <div className='question-content'>
+                                    <div className="col-md-6">
+                                        <input 
+                                        type="text" 
+                                        className="form-control"
+                                        value={question.description}
+                                        />
+                                    </div>
+                                    <div className="col-md-2">
+                                        <label className="form-label label-upload" htmlFor='labelUpload'><BiImageAdd className='icon-image'/>Upload File Image</label>
+                                        <input 
+                                        type='file' 
+                                        hidden
+                                        id='labelUpload'
+                                        />
+                                    </div>
+                                    <div className='col-md-5 btn-add'>
+                                        <span onClick={() => handleAddRemoveQuestion('ADD', '')}>
+                                            <BsPatchPlusFill className='icon-add'/>
+                                        </span>
+                                        {
+                                            questions && questions.length > 1 &&
+                                            <span onClick={() => handleAddRemoveQuestion('REMOVE', question.id)}>
+                                                <BsFillPatchMinusFill className='icon-delete'/>
+                                            </span>
+                                        }
+                                    </div>
+                                </div>
+                                {
+                                    question.answers && question.answers.length > 0
+                                    && question.answers.map((answer, index) => {
+                                        return(
+                                            <>
+                                                <div key={answer.id} className='answer-content mt-3'>
+                                                    <div className="form-check mt-1">
+                                                        <input className="form-check-input" 
+                                                            type="checkbox"
+                                                        />
+                                                    </div>
+                                                    <div className="col-md-6">
+                                                        <input
+                                                        type="text" 
+                                                        value={answer.description}
+                                                        className="form-control"
+                                                        />
+                                                    </div>
+                                                    <div className='col-md-5 btn-add'>
+                                                        <span onClick={() => handleAddRemoveAnswer('ADD', question.id, '')}>
+                                                            <BsPatchPlusFill className='icon-add'/>
+                                                        </span>
+                                                        {
+                                                            question.answers && question.answers.length > 1 &&
+                                                            <span onClick={() => handleAddRemoveAnswer('REMOVE', question.id, answer.id)}>
+                                                                <BsFillPatchMinusFill className='icon-delete'/>
+                                                            </span>
+                                                        }
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                }
             </div>
         </div>
     )
