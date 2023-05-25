b428b885855890f48c49071d52492e706ba6dc82
NamPV686 <nampv686@gmail.com>
2023-05-25 15:08:45 +0700

    #110. Design DashBoard

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -46,6 +46,7 @@ const DetailQuiz = (props) => {
                     answers.push(item.answers);
                 });
 
+                answers = _.orderBy(answers, ['id'], ['asc'])
                 return(
                     { questionId: key, answers: answers, questionDescription, image}
                 )
